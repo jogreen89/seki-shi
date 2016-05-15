75f4a1cdee99167b81c79132b9d6ee12f61f6187
zubernetes <zubernetes@gmail.com>
2016-05-15 17:13:01 -0400

    First C# program, nice and simple.

---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Small Greetings
+            Console.WriteLine("Hello, World.");
+
+            // Keep the console window open in debug mode.
+            // Console.WriteLine("Press any key to continue.");
+            // Console.ReadKey();  
+        }
+    }
+}
