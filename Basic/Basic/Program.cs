c1dc41cd3fe215e206c75a5d7c344f6f22c905f2
zubernetes <zubernetes@gmail.com>
2016-05-15 17:22:09 -0400

    Small modifications to basic class. Building new project.

--- Basic/Basic/Program.cs
+++ Basic/Basic/Program.cs
@@ -1,17 +1,15 @@
-﻿using System;
+﻿// Program.cs 
+// 
+// A basic Hello World written in C#
+// 2016 (c) zubernetes
+using System;
 
-namespace Basic
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Basic {
+    class Basic {
+        static void Main(string[] args) {
             // Small Greetings
             Console.WriteLine("Hello, World.");
-
-            // Keep the console window open in debug mode.
-            // Console.WriteLine("Press any key to continue.");
-            // Console.ReadKey();  
+            return; 
         }
     }
 }
