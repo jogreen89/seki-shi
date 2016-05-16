53c21450790ad6512ce52d2a0c6c7f08fbb16729
zubernetes <zubernetes@gmail.com>
2016-05-15 21:27:14 -0400

    New CLI program for working with the Employee table.

---
+++ EmployeeManager/EmployeeManager/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EmployeeManager {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World.");
+            return;
+        }
+    }
+}
