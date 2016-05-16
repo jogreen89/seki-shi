945a4e307537a042b57cdc578a98171c537b7f95
zubernetes <zubernetes@gmail.com>
2016-05-15 21:54:42 -0400

    Taking a little break, perhaps that's enough for tonight.

--- EmployeeManager/EmployeeManager/Program.cs
+++ EmployeeManager/EmployeeManager/Program.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 
-namespace EmployeeManager {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World.");
-            return;
+namespace EmployeeManager
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: EmployeeManager [OPTION]... DATA [ACTION]...");
+                Console.WriteLine("Try 'grep --help' for more information.");
+                return 1;
+            }
+            // 
+            Console.WriteLine("Welcome to Employee Manager.");
+
+            return 0;
         }
+    }
+    class Employee
+    {
+
     }
 }
